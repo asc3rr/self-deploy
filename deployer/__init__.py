d852c49985707e18cb95f4fcf71609def368ced0
asc3rr <borysgnacinski@gmail.com>
2020-11-18 14:26:21 +0100

    added repo syncing

--- deployer/__init__.py
+++ deployer/__init__.py
@@ -32,3 +32,16 @@ cd {cloner_dir}; git clone https://github.com/{self.user}/{cloner_repo}"""
             f = open(f"deployer/cloners/{cloner_repo}.sh", "w")
             f.write(cloner_content)
             f.close()
+
+    def sync_tracked_repos(self):
+        """Adds untracked repos (from config file) to `self.tracked_data`"""
+
+        untracked_repos = json.load(open(self.config_file))["tracked_data"]
+
+        ## Removing already tracked repos
+        for obj in self.tracked_data:
+            untracked_repos.remove(obj)
+
+        ## Updating list of tracked repos
+        for obj in untracked_repos:
+            self.tracked_data.append(obj)
